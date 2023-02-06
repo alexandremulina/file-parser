94b809e7f22d0b7ac0532846872858e574d32684
alexandremulina <alexandremulina@hotmail.com>
2023-02-06 16:39:02 -0300

    1.02

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,55 @@
 import * as fs from 'fs';
 
-export function fileParser(file: any, options: { delemiter: string; header: boolean }) {
-  switch (options.header) {
-    case true:
-      const list = [];
-      const result = fs.readFileSync(file, 'utf-8');
-      if (!result) {
-        throw 'Error ao abrir arquivo';
-      }
-      const dataArray = result.split(/\r?\n/);
-      const headers = dataArray[0].split(options.delemiter);
-      const body = dataArray.slice(1);
-
-      for (const line in body) {
-        if (body[line].split(';').length > 1) {
-          const row = body[line].split(';');
-          // console.log(`Header ${headers.length}`, `Row ${row.length}`);
-          if (headers.length != row.length) {
-            throw 'Error formato';
-          }
-          const newObj: any = {};
-          for (const value in headers) {
-            newObj[headers[value]] = row[value];
-          }
-          list.push(newObj);
-        }
-      }
-      if (list.length === 0) {
-        throw 'Error na formatação';
-      }
-      return list;
-
-    case false:
-      const noHeaderList = [];
-      console.log(options);
-      const noHeaderResult = fs.readFileSync(file, 'utf-8');
-      if (!noHeaderResult) {
-        throw 'Error ao abrir arquivo';
-      }
-      const noHeaderData = noHeaderResult.split(/\r?\n/);
-      for (const line in noHeaderData) {
-        if (noHeaderData[line].split(options.delemiter).length > 1) {
-          const row = noHeaderData[line].split(options.delemiter);
-          noHeaderList.push(row);
-        }
-      }
-
-      if (noHeaderList.length === 0) {
-        throw 'Error na formatação';
-      }
-
-      return noHeaderList;
+export function fileParser(file: any, options: { delimiter: string; header: boolean }) {
+  const result = fs.readFileSync(file, 'utf-8');
+  if (!result) {
+    throw 'Error ao abrir arquivo';
   }
+
+  const dataArray = result.split(/\r?\n/);
+  const list = options.header
+    ? parseWithHeader(dataArray, options.delimiter)
+    : parseWithoutHeader(dataArray, options.delimiter);
+
+  if (list.length === 0) {
+    throw 'Error na formatação';
+  }
+
+  return list;
+}
+
+function parseWithHeader(dataArray: string[], delimiter: string) {
+  const headers = dataArray[0].split(delimiter);
+  const body = dataArray.slice(1);
+  const list = [];
+
+  for (const line of body) {
+    if (line.split(delimiter).length > 1) {
+      const row = line.split(delimiter);
+      if (headers.length !== row.length) {
+        throw 'Error formato';
+      }
+
+      const newObj = {} as any;
+      for (let i = 0; i < headers.length; i++) {
+        newObj[headers[i]] = row[i];
+      }
+
+      list.push(newObj);
+    }
+  }
+
+  return list;
+}
+
+function parseWithoutHeader(dataArray: string[], delimiter: string) {
+  const list = [];
+
+  for (const line of dataArray) {
+    if (line.split(delimiter).length > 1) {
+      list.push(line.split(delimiter));
+    }
+  }
+
+  return list;
 }
